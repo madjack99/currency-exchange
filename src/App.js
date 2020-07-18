25afa385980ed21103f11bb1a92518ada9b4fbaf
Alex <kavanokami@gmail.com>
2020-07-18 21:39:51 +0400

    feat: add Exchange container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Exchange from './Exchange';
 
 function App() {
   return (
     <div>
       <Header />
+      <Exchange />
       <Footer />
     </div>
   );
---
+++ src/Exchange.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './exchange.css';
+
+const Exchange = () => {
+  return (
+    <div className='exchange'>
+      <span>Base</span>
+      <span>Change</span>
+      <span>Target</span>
+    </div>
+  );
+};
+
+export default Exchange;
