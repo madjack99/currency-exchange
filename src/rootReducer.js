34385fe5e8b5591163dc1e225b917a30d1a178e0
Alex <kavanokami@gmail.com>
2020-07-19 13:38:09 +0400

    feat: add rootReducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,3 +1,33 @@
-export default (state, action) {
-  
-}
+import {
+  GET_EXCHANGE_RATE,
+  SET_BASE_CURRENCY,
+  SET_TARGET_CURRENCY,
+} from './actionTypes';
+
+const initState = {
+  baseCurrency: null,
+  targetCurrencyArr: [],
+  exchangeRate: {},
+};
+
+export default (state = initState, action) => {
+  switch (action.payload) {
+    case SET_BASE_CURRENCY:
+      return {
+        ...state,
+        baseCurrency: action.payload,
+      };
+    case SET_TARGET_CURRENCY:
+      return {
+        ...state,
+        targetCurrencyArr: action.payload,
+      };
+    case GET_EXCHANGE_RATE:
+      return {
+        ...state,
+        exchangeRate: action.payload,
+      };
+    default:
+      return state;
+  }
+};
